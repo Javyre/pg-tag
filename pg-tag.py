b2fca20d1d1c12c3c35f553017f44cd8d3cecb98
Javier Pollak <javi.po.123@gmail.com>
2017-03-15 16:07:52 -0400

    Refactored code to please my editor...

--- pg-tag.py
+++ pg-tag.py
@@ -1,35 +1,44 @@
-import pygame
-import random
+#! /usr/bin/env python3
+import pygame as pg
 import sys
 import time
-from pygame.locals import *
-from Vec2 import *
+# from pygame.locals import *
+from Vec2 import Vec2d
 
 SW = 500
 SH = 500
 
-pygame.init()
+pg.init()
 
-screen = pygame.display.set_mode((SW, SH))
-pygame.display.set_caption("zach is sitting next to me right now")
+screen = pg.display.set_mode((SW, SH))
+pg.display.set_caption("zach is sitting next to me right now")
 
-class Player:
-    def __init__(self, x, y, w, h, speed=500, color=(255,0,0), controls=(K_UP, K_DOWN, K_RIGHT, K_LEFT)):
+
+class Player(object):
+    def __init__(self, x, y, w, h, speed=500, color=(255, 0, 0),
+                 controls=(pg.K_UP, pg.K_DOWN, pg.K_RIGHT, pg.K_LEFT)):
         self.x, self.y = x, y
         self.w, self.h = w, h
         self.color = color
         self.speed = speed
-        self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.rect = pg.Rect(self.x, self.y, self.w, self.h)
         self.ku, self.kd, self.kr, self.kl = controls
-        self.directions = {"up": (0, -self.speed), "down": (0, self.speed), "left": (-self.speed, 0), "right": (self.speed, 0)}
-        self.displacement = Vec2d(0,0)
+        self.directions = {"up":    (0, -self.speed),
+                           "down":  (0, self.speed),
+                           "left":  (-self.speed, 0),
+                           "right": (self.speed, 0)}
+        self.displacement = Vec2d(0, 0)
+
     def move(self, direction, dtime):
         self.displacement += direction*dtime
-        self.rect = self.rect.move(int(self.displacement.x), int(self.displacement.y))
-        self.displacement -= (int(self.displacement.x), int(self.displacement.y))
+        self.rect = self.rect.move(int(self.displacement.x),
+                                   int(self.displacement.y))
+        self.displacement -= (int(self.displacement.x),
+                              int(self.displacement.y))
+
     def control(self, dtime):
-        keys = pygame.key.get_pressed()
-        direction = Vec2d(0,0)
+        keys = pg.key.get_pressed()
+        direction = Vec2d(0, 0)
         if keys[self.ku]:
             direction += self.directions["up"]
         if keys[self.kd]:
@@ -39,15 +48,15 @@ class Player:
         if keys[self.kr]:
             direction += self.directions["right"]
         self.move(direction, dtime)
+
     def update(self, screen, dtime):
         self.control(dtime)
-        pygame.draw.rect(screen, self.color, self.rect)
-
+        pg.draw.rect(screen, self.color, self.rect)
 
 
 class game (object):
     def run(self):
-        p1 = Player(50, 50, 50, 100, controls=(K_w, K_s, K_d, K_a))
+        p1 = Player(50, 50, 50, 100, controls=(pg.K_w, pg.K_s, pg.K_d, pg.K_a))
         p2 = Player(50, 50, 100, 50, color=(0, 0, 255))
         players = [p1, p2]
         running = True
@@ -55,22 +64,22 @@ class game (object):
         stime = 0
         while running:
             stime = time.time()
-            keys = pygame.key.get_pressed()
-            screen.fill((0,0,0))
+            screen.fill((0, 0, 0))
 
-            p1.move(Vec2d(300,200), dtime)
+            p1.move(Vec2d(300, 200), dtime)
             for player in players:
                 player.update(screen, dtime)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     running = False
-            pygame.display.flip()
+            pg.display.flip()
             dtime = time.time()-stime
-        # pygame.time.delay(20)
+        # pg.time.delay(20)
 
-        pygame.quit()
+        pg.quit()
         sys.exit()
+
 
 if __name__ == "__main__":
     game().run()
